fbabaf942bb09517f32dabbe0abd0492e9b93c32
arjundatt <arjundatt.16@gmail.com>
2015-10-13 17:05:29 -0400

    Project init attempt 2

--- Global Ids/Meta database/stopmetadb.bat
+++
@@ -1,1 +0,0 @@
-".\..\jdk\bin\java.exe" -Xms128m -Xmx512m -Djavax.net.ssl.keyStore=internalDBStore.key -Djavax.net.ssl.keyStorePassword=GlobalidsDB2013 -cp ../lib/drivers/derbynet.jar;../lib/drivers/derby.jar;../lib/drivers/derbyclient.jar  org.apache.derby.drda.NetworkServerControl shutdown -h 0.0.0.0 -p 1530 -ssl basic
